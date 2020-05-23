1445266ef97f8b8a5fc8703f1e9a240c12862a8e
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-05-23 18:04:47 +0900

    Initial Commit

---
+++ GitHubRepoList-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubRepoList-SwiftUI
+//
+//  Created by Satoshi Watanabe on 2020/05/23.
+//  Copyright © 2020 Satoshi Watanabe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubRepoList-SwiftUITests/GitHubRepoList_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHubRepoList_SwiftUITests.swift
+//  GitHubRepoList-SwiftUITests
+//
+//  Created by Satoshi Watanabe on 2020/05/23.
+//  Copyright © 2020 Satoshi Watanabe. All rights reserved.
+//
+
+import XCTest
+@testable import GitHubRepoList_SwiftUI
+
+class GitHubRepoList_SwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
